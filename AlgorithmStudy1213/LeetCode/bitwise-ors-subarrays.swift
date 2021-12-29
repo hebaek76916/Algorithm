69754a7a7e7e2e65e23b9178e5f24ee79bc8d302
hebaek76916 <qorgusdms15@gmail.com>
2021-12-30 00:25:34 +0900

    LeetCode - bit-ors-subarrays : incomplete

---
+++ AlgorithmStudy1213/LeetCode/bitwise-ors-subarrays.swift
@@ -0,0 +1,52 @@
+//
+//  bitwise-ors-subarrays.swift
+//  AlgorithmStudy1213
+//
+//  Created by 현은백 on 2021/12/29.
+//
+
+import Foundation
+//https://leetcode.com/problems/bitwise-ors-of-subarrays/
+
+func sol(_ arr: [Int]) -> Int {
+    
+    for i in 0..<arr.count {
+        var set = Set<Int>()
+        
+    }
+    
+    return 0
+}
+
+let arr = [0]
+print(sol(Array(Set(arr))))
+
+//func sol(_ arr: [Int]) -> Int {
+//    if arr.count == 1 {
+//        return 1
+//    }
+//
+//    var subSet: [[Int]] = [[]]
+//    var tempSaver: [Int: Int] = [:]
+//
+//    for i in 0..<arr.count {
+//        for j in 0..<subSet.count {
+//            subSet.append(subSet[j] + [arr[i]])
+//        }
+//    }
+//
+//    var set = Set<Int>()
+//
+//    subSet.forEach { sub in
+//        var base = 0
+//        print(sub)
+//        sub.forEach { num in
+//            print(num)
+//            base |= num
+//        }
+//        set.insert(base)
+//        print("BASE\(base)")
+//    }
+//    print(set)
+//    return set.count - 1
+//}
