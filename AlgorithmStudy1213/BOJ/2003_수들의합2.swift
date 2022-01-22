24b6a8e91626a67f6c460972aadb744e652c6f7e
hebaek76916 <qorgusdms15@gmail.com>
2022-01-23 08:45:10 +0900

    백준_2003_수들의합_TwoPointer

---
+++ AlgorithmStudy1213/BOJ/2003_수들의합2.swift
@@ -0,0 +1,8 @@
+//
+//  2003_수들의합2.swift
+//  AlgorithmStudy1213
+//
+//  Created by 현은백 on 2022/01/23.
+//
+
+import Foundation
--- AlgorithmStudy1213/main.swift
+++ AlgorithmStudy1213/main.swift
@@ -5,3 +5,29 @@
 //  Created by 현은백 on 2021/12/13.
 //
 import Foundation
+func sol(N: Int, num: Int) {
+    let arr = readLine()!.split(separator: " ").compactMap{ Int($0) }
+    var sum = 0
+    var j = 0
+    var cnt = 0
+    for i in 0..<N {
+        
+        sum += arr[i]
+        print("SUM", sum)
+        if sum == num {
+            cnt += 1
+        }
+        while sum >= num && j < N {
+            sum -= arr[j]
+            j += 1
+            if sum == num {
+                cnt += 1
+            }
+        }
+    }
+    print(cnt)
+}
+if let input = readLine() {
+    let arr = input.split(separator: " ").compactMap{ Int($0)! }
+    sol(N: arr[0], num: arr[1])
+}
