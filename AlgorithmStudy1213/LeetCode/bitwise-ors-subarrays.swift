c92640d884fd5c113c848957fb340ce00ba4132b
hebaek76916 <qorgusdms15@gmail.com>
2022-01-24 17:26:29 +0900

    key : Two Pointer

--- AlgorithmStudy1213/LeetCode/bitwise-ors-subarrays.swift
+++ AlgorithmStudy1213/LeetCode/bitwise-ors-subarrays.swift
@@ -8,45 +8,23 @@
 import Foundation
 //https://leetcode.com/problems/bitwise-ors-of-subarrays/
 
-func sol(_ arr: [Int]) -> Int {
-    
-    for i in 0..<arr.count {
-        var set = Set<Int>()
+class Solution {
+    func subarrayBitwiseORs(_ arr: [Int]) -> Int {
+        var res: [Int] = []
+        var left = 0
+        var right = 0
         
+        for num in arr {
+            right = res.count
+            res.append(num)
+            for i in left..<right {
+                if res.last! != (res[i] | num) {
+                    res.append(res[i] | num)
+                }
+            }
+            print("\(left) \(right),res : \(res)")
+            left = right
+        }
+        return Set(res).count
     }
-    
-    return 0
 }
-
-let arr = [0]
-
-
-//func sol(_ arr: [Int]) -> Int {
-//    if arr.count == 1 {
-//        return 1
-//    }
-//
-//    var subSet: [[Int]] = [[]]
-//    var tempSaver: [Int: Int] = [:]
-//
-//    for i in 0..<arr.count {
-//        for j in 0..<subSet.count {
-//            subSet.append(subSet[j] + [arr[i]])
-//        }
-//    }
-//
-//    var set = Set<Int>()
-//
-//    subSet.forEach { sub in
-//        var base = 0
-//        print(sub)
-//        sub.forEach { num in
-//            print(num)
-//            base |= num
-//        }
-//        set.insert(base)
-//        print("BASE\(base)")
-//    }
-//    print(set)
-//    return set.count - 1
-//}
