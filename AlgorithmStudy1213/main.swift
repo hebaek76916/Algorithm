39040a33e8e1d14f7d590ddce48aaa209cac5df9
hebaek76916 <qorgusdms15@gmail.com>
2022-01-25 22:21:42 +0900

    확장 유클리드 알고리즘

--- AlgorithmStudy1213/main.swift
+++ AlgorithmStudy1213/main.swift
@@ -5,32 +5,4 @@
 ////  Created by 현은백 on 2021/12/13.
 ////
 import Foundation
-protocol Serializer {
-    func serialize(data: AnyObject) -> Int?
-}
 
-class RequestSerializer: Serializer {
-
-    func serialize(data: AnyObject) -> Int? {return nil}
-
-}
-
-class DataManager {
-    var serializer: Serializer? = RequestSerializer()
-}
-
-func isPrime(_ num: Int) -> Bool {
-    if num <= 1 { return false }
-    if num == 2 { return true }
-    
-    if num % 2 == 0 { return false }
-    let sqrtn = Int(sqrt(Double(num)))
-    for div in stride(from: 3, through: sqrtn, by: 2) {
-        if num % div == 0 {
-            return false
-        }
-    }
-    return true
-}
-
-print(isPrime(433))
